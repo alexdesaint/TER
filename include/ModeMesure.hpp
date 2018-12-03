94c5574f9e948f81cdefed5ac2ccc53a7651c668
OnlyShoky <38210747+OnlyShoky@users.noreply.github.com>
2018-12-03 13:15:13 +0100

    Update Mode Mesure

--- include/ModeMesure.hpp
+++ include/ModeMesure.hpp
@@ -15,6 +15,9 @@ private:
   // Bool pause , mode 1 et mode 2 sont les boolean qui servent a savoir si on est respectivement dans la pause , mode 1 ou mode 2
   bool boolPause , boolMode1 , boolMode2; 
 
+  bool boolPresencePersonne = false ;
+  bool boolBouton = false ; 
+
 
   std::array<uint64_t, 10> tabTemps ;
   int indiceTabTemps;
@@ -23,10 +26,18 @@ private:
 public:
   uint64_t lectureTemps();
   void init();
-  void mode1(bool capteurLaser);
 
+  void mode1();
   // On return un boolean pour savoir si on est encore entrain de traité ou on peut passer au mode 1
-  bool mode2(bool capteurLaser);
+  bool mode2();
+
+  void absencePersonne();
+  bool presencePersonne();
+
+  void presenceBouton();
+  void absenceBouton();
+
+
 
   uint64_t getTime();
 };
